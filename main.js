a9267281e0a32acccc0f40930e8d79c20612ddf6
Opher Vishnia <opherv@gmail.com>
2016-06-16 22:35:42 +0300

    Removed spaaace examples, made into proper NPM package

--- main.js
+++ main.js
@@ -1,47 +1,8 @@
-'use strict';
+var Incheon = {
+    GameEngine: require("./src/GameEngine"),
+    ServerEngine: require("./src/ServerEngine"),
+    ClientEngine: require("./src/ClientEngine"),
+    GameWorld: require("./src/GameWorld")
+};
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'examples/spaaace/index.html');
-
-const server = express();
-
-server.get('/', function (req, res) {
-    res.sendFile(INDEX)
-});
-
-
-server.use('/', express.static(path.join(__dirname, 'examples/spaaace')));
-
-var requestHandler = server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(requestHandler);
-
-
-/*
-    Game logic
- */
-
-const SpaaaceServerEngine = require(path.join(__dirname, 'examples/spaaace/js/SpaaaceServerEngine.js'));
-const SpaaaceGameEngine = require(path.join(__dirname, 'examples/spaaace/js/SpaaaceGameEngine.js'));
-
-const gameEngine = new SpaaaceGameEngine();
-const serverEngine = new SpaaaceServerEngine(io, gameEngine);
-serverEngine.start();
-
-/*
-    Server IO
- */
-
-io.on('connection', onClientConnect);
-
-function onClientConnect(socket){
-    serverEngine.onPlayerConnected(socket);
-}
-
-
-
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+module.exports = Incheon;
