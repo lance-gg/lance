c06e577f53111e0eab5c2ad888b8bd60caac3b95
Gary Weiss <garyweiss74@gmail.com>
2016-09-06 19:14:52 +0000

    limit velocity

--- src/serialize/DynamicObject.js
+++ src/serialize/DynamicObject.js
@@ -105,6 +105,7 @@ class DynamicObject extends Serializable {
             this.velocity.x = Math.round(this.velocity.x * 100) / 100;
             this.velocity.y = Math.round(this.velocity.y * 100) / 100;
         }
+        if (this.velocity.getMagnitude() > this.maxSpeed) this.velocity.setMagnitude(this.maxSpeed);
 
         this.velX = this.velocity.x;
         this.velY = this.velocity.y;
