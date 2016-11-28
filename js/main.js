968964cf76110fcd996c17e0309ac5c6fa27fa34
Opher Vishnia <opherv@gmail.com>
2016-11-28 23:53:48 +0200

    initial commit: splash

---
+++ js/main.js
@@ -0,0 +1,2 @@
+
+require("!style-loader!css-loader!sass-loader!./../sass/main.scss");
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+    entry: "./js/main.js",
+    output: {
+        path: __dirname+"/dist_bundles",
+        publicPath: "/dist_bundles",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+            {
+                test: /.*\.(gif|png|jpe?g|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack'
+                ]
+            }
+        ]
+    },
+    sassLoader: {
+        includePaths: [
+            path.resolve(__dirname, "./node_modules/compass-mixins/lib"),
+            path.resolve(__dirname, "./node_modules/breakpoint-sass/stylesheets")
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ],
+
+    devtool: "source-map"
+};
