c89a8633888fcedf0724c43ce8da4dca0eb323bc
Opher Vishnia <opherv@gmail.com>
2016-12-16 22:12:37 +0200

    comment out PhysijsPhysicsEngine

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ var Incheon = {
     },
     physics: {
         PhysicsEngine: require("./src/physics/PhysicsEngine"),
-        SimplePhysicsEngine: require("./src/physics/SimplePhysicsEngine"),
-        PhysijsPhysicsEngine: require("./src/physics/PhysijsPhysicsEngine")
+        SimplePhysicsEngine: require("./src/physics/SimplePhysicsEngine")
+        // PhysijsPhysicsEngine: require("./src/physics/PhysijsPhysicsEngine")
     },
     render: {
         Renderer: require("./src/render/Renderer"),
