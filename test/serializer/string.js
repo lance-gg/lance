91fe5ab768cf8382083fcac794868fb2d9dc3a7f
Gary Weiss <fourvelocity@gmail.com>
2017-05-25 15:02:39 +0300

    add test for string serialization

---
+++ test/serializer/string.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const should = require('should');
+
+const Serializable = require('../../src/serialize/Serializable');
+const Serializer = require('../../src/serialize//Serializer');
+
+class TestObject extends Serializable {
+
+    static get netScheme() {
+        return {
+            helloString: { type: Serializer.TYPES.STRING },
+            color: { type: Serializer.TYPES.STRING }
+        };
+    }
+
+    constructor(helloString) {
+        super();
+        this.helloString = helloString;
+        this.color = 'RED';
+    }
+}
+
+var serializer = new Serializer();
+
+serializer.registerClass(TestObject);
+let testObjects = [new TestObject('hello'), new TestObject('hello'), new TestObject('goodbye')];
+testObjects.map(t => {
+    t.class = TestObject;
+    return null;
+});
+
+describe('List serialization/deserialization', function() {
+    let serializedTestObjects;
+    let deserializedTestObjects;
+
+    describe('primitives', function() {
+
+        it('Serialize object with string', function() {
+            serializedTestObjects = testObjects.map(t => { return t.serialize(serializer); });
+        });
+
+        it('Deserialize list', function() {
+            deserializedTestObjects = serializedTestObjects.map(s => { return serializer.deserialize(s.dataBuffer); });
+            deserializedTestObjects[0].obj.helloString.should.equal('hello');
+            deserializedTestObjects[1].obj.helloString.should.equal('hello');
+            deserializedTestObjects[2].obj.helloString.should.equal('goodbye');
+        });
+
+    });
+
+});
