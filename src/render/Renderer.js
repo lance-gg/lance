2fec6cead3f4b75b6fd7ee6f77111cb6d4b6c9a1
Gary Weiss <fourvelocity@gmail.com>
2017-06-07 10:57:57 +0300

    add renderer schedule logic

--- src/render/Renderer.js
+++ src/render/Renderer.js
@@ -2,6 +2,8 @@
 
 const EventEmitter = require('eventemitter3');
 
+const TIME_RESET_THRESHOLD = 100;
+
 /**
  * The Renderer is the component which must *draw* the game on the client.
  * It will be instantiated once on each client, and must implement the draw
@@ -18,6 +20,7 @@ class Renderer {
     constructor(gameEngine, clientEngine) {
         this.gameEngine = gameEngine;
         this.clientEngine = clientEngine;
+        this.gameEngine.on('client__stepReset', () => { this.doReset = true; });
         gameEngine.on('objectAdded', this.addObject.bind(this));
         gameEngine.on('objectDestroyed', this.removeObject.bind(this));
 
@@ -43,12 +46,69 @@ class Renderer {
     /**
      * The main draw function.  This method is called at high frequency,
      * at the rate of the render loop.  Typically this is 60Hz, in WebVR 90Hz.
+     * If the client engine has been configured to render-schedule, then this
+     * method must call the clientEngine's step method.
+     *
+     * @param {Number} t - current time (only required in render-schedule mode)
+     * @param {Number} dt - time elapsed since last draw (only required in render-schedule mode)
      */
-    draw() {
-        this.gameEngine.world.forEachObject((id, o) => {
-            if (typeof o.refreshRenderObject === 'function')
-                o.refreshRenderObject();
-        });
+    draw(t, dt) {
+        if (this.clientEngine.options.scheduler === 'render-schedule')
+            this.runClientStep(t, dt);
+    }
+
+    /**
+     * The main draw function.  This method is called at high frequency,
+     * at the rate of the render loop.  Typically this is 60Hz, in WebVR 90Hz.
+     *
+     * @param {Number} t - current time
+     * @param {Number} dt - time elapsed since last draw
+     */
+    runClientStep(t, dt) {
+        let p = this.clientEngine.options.stepPeriod;
+
+        if (this.doReset || t > this.clientEngine.lastStepTime + TIME_RESET_THRESHOLD) {
+            this.doReset = false;
+            this.clientEngine.lastStepTime = t - p / 2;
+            this.clientEngine.correction = p / 2;
+// HACK: remove next line
+            this.clientEngine.gameEngine.trace.trace(`============RESETTING lastTime=${this.clientEngine.lastStepTime} period=${p}`);
+        }
+
+        // catch-up missed steps
+        while (t > this.clientEngine.lastStepTime + p) {
+// HACK: remove next line
+this.clientEngine.gameEngine.trace.trace(`============RENDERER DRAWING EXTRA t=${t} LST=${this.clientEngine.lastStepTime} correction = ${this.clientEngine.correction} period=${p}`);
+            this.clientEngine.step(this.clientEngine.lastStepTime + p, p + this.clientEngine.correction);
+            this.clientEngine.lastStepTime += p;
+            this.clientEngine.correction = 0;
+        }
+
+        // not ready for a real step yet
+        // might happen after catch up above
+        if (t < this.clientEngine.lastStepTime) {
+// HACK: remove next line
+            this.clientEngine.gameEngine.trace.trace(`============RENDERER DRAWING NOSTEP t=${t} dt=${t - this.clientEngine.lastStepTime} correction = ${this.clientEngine.correction} period=${p}`);
+
+            dt = t - this.clientEngine.lastStepTime + this.clientEngine.correction;
+            if (dt < 0) dt = 0;
+            this.clientEngine.correction = this.clientEngine.lastStepTime - t;
+            this.clientEngine.step(t, dt, true);
+            return;
+        }
+
+        // render-controlled step
+
+// HACK: remove next line
+        this.clientEngine.gameEngine.trace.trace(`============RENDERER DRAWING t=${t} LST=${this.clientEngine.lastStepTime} correction = ${this.clientEngine.correction} period=${p}`);
+
+        dt = t - this.clientEngine.lastStepTime + this.clientEngine.correction;
+        this.clientEngine.lastStepTime += p;
+        this.clientEngine.correction = this.clientEngine.lastStepTime - t;
+        this.clientEngine.step(t, dt);
+
+// HACK: remove next line
+this.clientEngine.gameEngine.trace.trace(`============RENDERER DONE t=${t} LST=${this.clientEngine.lastStepTime} correction = ${this.clientEngine.correction} period=${p}`);
     }
 
     /**
