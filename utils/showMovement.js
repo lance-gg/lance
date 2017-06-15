09eba9964f7554bd16b45b5556fc47d508740b08
Gary Weiss <fourvelocity@gmail.com>
2017-06-15 10:06:51 +0300

    show time delta in debug tool

--- utils/showMovement.js
+++ utils/showMovement.js
@@ -6,6 +6,7 @@ const process = require('process');
 //
 let FILENAME = 'server.trace';
 let positions = {};
+let times = {};
 
 if (process.argv.length === 3) FILENAME = process.argv[2];
 let fin = fs.readFileSync(FILENAME);
@@ -14,12 +15,17 @@ for (let l of lines) {
     if (l.indexOf('after step') < 0) continue;
 
     let p = l.match(/Pos=\(([0-9.-]*), ([0-9.-]*), ([0-9.-]*)\)/);
+    let t = l.match(/^\[([0-9\-T:.Z]*)\]/); // 2017-06-01T14:25:24.197Z
+    let ts = new Date(t[1]);
     let step = l.match(/([0-9]*>)/);
     let parts = l.split(' ');
     let objname = parts[4];
     let oldp = positions[objname];
+    let oldts = times[objname];
     if (oldp) {
-        console.log(`step ${step[1]} object ${objname} moved (${Number(p[1]) - Number(oldp[1])},${Number(p[2]) - Number(oldp[2])},${Number(p[3]) - Number(oldp[3])})}`);
+        let delta = `(${Number(p[1]) - Number(oldp[1])},${Number(p[2]) - Number(oldp[2])},${Number(p[3]) - Number(oldp[3])})`;
+        console.log(`step ${step[1]} dt=${ts - oldts} object ${objname} moved ${delta}`);
     }
     positions[objname] = p;
+    times[objname] = ts;
 }
