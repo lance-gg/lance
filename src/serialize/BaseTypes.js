441c79a69e8689a785e06c81948b831446b7d435
Gary Weiss <fourvelocity@gmail.com>
2018-05-06 00:39:15 +0300

    add BaseTypes.js

---
+++ src/serialize/BaseTypes.js
@@ -0,0 +1,15 @@
+
+class BaseTypes {}
+
+BaseTypes.TYPES = {
+    FLOAT32: 'FLOAT32',
+    INT32: 'INT32',
+    INT16: 'INT16',
+    INT8: 'INT8',
+    UINT8: 'UINT8',
+    STRING: 'STRING',
+    CLASSINSTANCE: 'CLASSINSTANCE',
+    LIST: 'LIST'
+};
+
+export default BaseTypes;
