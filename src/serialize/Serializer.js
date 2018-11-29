6194902a2c8cbd6b0e8597e8268caa768f1bc63a
dev-zetta <dev.zetta@gmail.com>
2018-11-29 01:02:32 +0100

    Serialize algorithm should be in the Serializer class

--- src/serialize/Serializer.js
+++ src/serialize/Serializer.js
@@ -56,6 +56,116 @@ class Serializer {
         this.registeredClasses[classId] = classObj;
     }
 
+    /**
+     *  Class can be serialized using either:
+     * - a class based netScheme
+     * - an instance based netScheme
+     * - completely dynamically (not implemented yet)
+     *
+     * @param {Object} object - Object to be serialized
+     * @param {Object} [options] - Options object
+     * @param {Object} options.dataBuffer [optional] - Data buffer to write to. If null a new data buffer will be created
+     * @param {Number} options.bufferOffset [optional] - The buffer data offset to start writing at. Default: 0
+     * @param {String} options.dry [optional] - Does not actually write to the buffer (useful to gather serializeable size)
+     * @return {Object} the serialized object.  Contains attributes: dataBuffer - buffer which contains the serialized data;  bufferOffset - offset where the serialized data starts.
+     */
+    serialize(object, options) {
+        options = Object.assign({
+            bufferOffset: 0
+        }, options);
+
+        let netScheme;
+        let dataBuffer;
+        let dataView;
+        let classId = 0;
+        let bufferOffset = options.bufferOffset;
+        let localBufferOffset = 0; // used for counting the bufferOffset
+
+        // instance classId
+        if (object.classId) {
+            classId = object.classId;
+        } else {
+            classId = Utils.hashStr(object.constructor.name);
+        }
+
+        // instance netScheme
+        if (object.netScheme) {
+            netScheme = object.netScheme;
+        } else if (object.constructor.netScheme) {
+            netScheme = object.constructor.netScheme;
+        } else {
+            // todo define behaviour when a netScheme is undefined
+            console.warn('no netScheme defined! This will result in awful performance');
+        }
+
+        // TODO: currently we serialize every node twice, once to calculate the size
+        //       of the buffers and once to write them out.  This can be reduced to
+        //       a single pass by starting with a large (and static) ArrayBuffer and
+        //       recursively building it up.
+        // buffer has one Uint8Array for class id, then payload
+        if (options.dataBuffer == null && options.dry != true) {
+            let bufferSize = object.serialize(this, { dry: true }).bufferOffset;
+            dataBuffer = new ArrayBuffer(bufferSize);
+        } else {
+            dataBuffer = options.dataBuffer;
+        }
+
+        if (options.dry != true) {
+            dataView = new DataView(dataBuffer);
+            // first set the id of the class, so that the deserializer can fetch information about it
+            dataView.setUint8(bufferOffset + localBufferOffset, classId);
+        }
+
+        // advance the offset counter
+        localBufferOffset += Uint8Array.BYTES_PER_ELEMENT;
+
+        if (netScheme) {
+            for (let property of Object.keys(netScheme).sort()) {
+
+                // write the property to buffer
+                if (options.dry != true) {
+                    this.writeDataView(dataView, object[property], bufferOffset + localBufferOffset, netScheme[property]);
+                }
+
+                if (netScheme[property].type === BaseTypes.TYPES.STRING) {
+                    // derive the size of the string
+                    localBufferOffset += Uint16Array.BYTES_PER_ELEMENT;
+                    if (object[property] !== null)
+                        localBufferOffset += object[property].length * Uint16Array.BYTES_PER_ELEMENT;
+                } else if (netScheme[property].type === BaseTypes.TYPES.CLASSINSTANCE) {
+                    // derive the size of the included class
+                    let objectInstanceBufferOffset = object[property].serialize(this, { dry: true }).bufferOffset;
+                    localBufferOffset += objectInstanceBufferOffset;
+                } else if (netScheme[property].type === BaseTypes.TYPES.LIST) {
+                    // derive the size of the list
+                    // list starts with number of elements
+                    localBufferOffset += Uint16Array.BYTES_PER_ELEMENT;
+
+                    for (let item of object[property]) {
+                        // todo inelegant, currently doesn't support list of lists
+                        if (netScheme[property].itemType === BaseTypes.TYPES.CLASSINSTANCE) {
+                            let listBufferOffset = item.serialize(this, { dry: true }).bufferOffset;
+                            localBufferOffset += listBufferOffset;
+                        } else if (netScheme[property].itemType === BaseTypes.TYPES.STRING) {
+                            // size includes string length plus double-byte characters
+                            localBufferOffset += Uint16Array.BYTES_PER_ELEMENT * (1 + item.length);
+                        } else {
+                            localBufferOffset += this.getTypeByteSize(netScheme[property].itemType);
+                        }
+                    }
+                } else {
+                    // advance offset
+                    localBufferOffset += this.getTypeByteSize(netScheme[property].type);
+                }
+
+            }
+        } else {
+            // TODO no netScheme, dynamic class
+        }
+
+        return { dataBuffer, bufferOffset: localBufferOffset };
+    }
+
     deserialize(dataBuffer, byteOffset) {
         byteOffset = byteOffset ? byteOffset : 0;
         let localByteOffset = 0;
