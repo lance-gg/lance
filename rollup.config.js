cbcf5d77f925b9d712ec726d07ee2f62d1bbd467
namel <fourvelocity@gmail.com>
2019-03-05 00:15:37 +0200

    config cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,25 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import pjson from 'rollup-plugin-json';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default [
     {
         // server build
         input: 'src/package/serverExports.js',
         external: ['fs', 'bufferutil', 'utf-8-validate'],
-        output: { file: 'dist/server/lance-gg.js', format: 'cjs', name: 'Server' },
+        output: { file: 'dist/server/lance-gg.js', format: 'esm', name: 'Server' },
         plugins: [
             resolve(),
-            babel({ exclude: '**/node_modules/**' }),
             pjson(),
             commonjs({ include: 'node_modules/**', extensions: ['.js'] })
         ]
     },
     {
+        // client build
         input: 'src/package/clientExports.js',
         output: { file: 'dist/client/lance-gg.js', format: 'umd', name: 'Client' },
         plugins: [
             resolve({ browser: true, preferBuiltins: false }),
-            babel({ exclude: '**/node_modules/**' }),
             pjson(),
             commonjs({ include: 'node_modules/**', extensions: ['.js'] })
         ]
