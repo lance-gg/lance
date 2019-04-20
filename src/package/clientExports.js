fa5d54632ef43c4280a7e9aeaf023aa74a599b96
Alex Anderson <alexanderson1993@gmail.com>
2019-04-20 15:17:00 -0600

    Update exports to include 3D primitives.

--- src/package/clientExports.js
+++ src/package/clientExports.js
@@ -4,6 +4,8 @@ import P2PhysicsEngine from '../physics/P2PhysicsEngine.js';
 import SimplePhysicsEngine from '../physics/SimplePhysicsEngine.js';
 import BaseTypes from '../serialize/BaseTypes.js';
 import TwoVector from '../serialize/TwoVector.js';
+import ThreeVector from '../serialize/ThreeVector.js';
+import Quaternion from '../serialize/Quaternion.js';
 import DynamicObject from '../serialize/DynamicObject.js';
 import PhysicalObject2D from '../serialize/PhysicalObject2D.js';
 import PhysicalObject3D from '../serialize/PhysicalObject3D.js';
@@ -19,6 +21,8 @@ export {
   SimplePhysicsEngine,
   BaseTypes,
   TwoVector,
+  ThreeVector,
+  Quaternion,
   DynamicObject,
   PhysicalObject2D,
   PhysicalObject3D,
